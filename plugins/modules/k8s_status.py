b3a25db0e867d2cf37a076e75bd20317699a0634
Ish Shah <ishah@redhat.com>
2021-01-08 19:18:53 -0800

    re-add argument

--- plugins/modules/k8s_status.py
+++ plugins/modules/k8s_status.py
@@ -223,7 +223,7 @@ def main():
 class KubernetesAnsibleStatusModule(KubernetesAnsibleModule):
     def __init__(self, *args, **kwargs):
         KubernetesAnsibleModule.__init__(
-            self, *args, **kwargs
+            self, *args, supports_check_mode=True, **kwargs
         )
         if not HAS_KUBERNETES_COLLECTION:
             self.fail_json(
